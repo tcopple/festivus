b03a7ada04a6800e3e8f00843dc5cbd14c5ec7be
Tyler Copple <j.tylercopple@gmail.com>
2013-08-16 21:34:33 -0400

    Adding questionnaire definition to Questionnaire#show

--- app/app/models/questionnaire.rb
+++ app/app/models/questionnaire.rb
@@ -1,19 +1,15 @@
 class Questionnaire < ActiveRecord::Base
   attr_accessible :description, :title
+  attr_accessor :definition
+
   has_many :questionnaire_instances, dependent: :destroy
 
   def survey_path
-    filename = title.gsub(' ', '').underscore.gsub('_', '-')
-    Rails.root.join('surveys', filename)
+    warn "[DEPRECATION] `survey_path` is deprecated. Please use `QuestionnaireServices.questionnaire_path` instead."
+    QuestionnaireServices.path(self)
   end
 
-  def parse_survey
-    survey = File.read(this.survey_path)
+  def to_text
+    self.definition ||= File.read(QuestionnaireServices.path(self))
   end
-
-  # whenever a survey is updated it shoudl be reparsed regarding the database.
-#   def save
-#     require 'surveyor'
-#     Surveyor::Parser.parse_file(survey_path)
-#   end
 end
--- app/lib/services/questionnaire_services.rb
+++ app/lib/services/questionnaire_services.rb
@@ -1,6 +1,11 @@
 module QuestionnaireServices
-  def self.parse_questionnaire questionnaire
+  def self.parse questionnaire
     #TODO add validation to make sure the survey has actually been updated before parsing
     Surveyor::Parser.parse_file(questionnaire.survey_path)
+  end
+
+  def self.path questionnaire
+    filename = questionnaire.title.gsub(' ', '').underscore.gsub('_', '-')
+    Rails.root.join('surveys', filename)
   end
 end
