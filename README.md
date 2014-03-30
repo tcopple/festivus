740309b3b5680189b9b14c2a4c0e653dcdbe1e7e
Tyler Copple <j.tylercopple@gmail.com>
2014-03-29 23:09:21 -0400

    Removing Vagrant setup from repository in favor of it's own repository.

--- README.md
+++
@@ -1,4 +0,0 @@
-festivus
-========
-
-A web app to help festival coordinators manage their band and venue workflows.
--- app/app/assets/javascripts/surveyor_all.js
+++
@@ -1,4 +0,0 @@
-//= require surveyor/jquery-ui-1.10.0.custom
-//= require surveyor/jquery.selectToUISlider
-//= require surveyor/jquery.surveyor
-//= require surveyor/jquery.maskedinput
